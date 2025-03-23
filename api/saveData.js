a501ea6348e4f98b5325e0843f5cf649e29a7872
apostolos-HEC <apo.b.21@windowslive.com>
2025-03-23 18:44:36 +0200

    Update saveData.js

--- api/saveData.js
+++ api/saveData.js
@@ -1,24 +1,14 @@
-// api/saveData.js
-
-const fs = require('fs');
+const express = require('express');
 const path = require('path');
 
-export default function handler(req, res) {
-  if (req.method === 'POST') {
-    const formData = req.body;
+const app = express();
 
-    // Αποθήκευση δεδομένων σε αρχείο .txt
-    const filePath = path.join(__dirname, '..', 'formData.txt');
-    const dataToSave = `First Name: ${formData.firstName}, Last Name: ${formData.lastName}, Position: ${formData.position}, Company: ${formData.company}\n`;
+app.use(express.static(path.join(__dirname, '../public')));
 
-    fs.appendFile(filePath, dataToSave, (err) => {
-      if (err) {
-        console.error('Error writing to file:', err);
-        return res.status(500).json({ message: 'Error writing to file' });
-      }
-      res.status(200).json({ message: 'Data saved successfully' });
-    });
-  } else {
-    res.status(405).json({ message: 'Method Not Allowed' });
-  }
-}
+// Σερβίρει το PDF
+app.get('/greek.pdf', (req, res) => {
+    res.sendFile(path.join(__dirname, '../greek.pdf'));
+});
+
+// Εξάγει την Express εφαρμογή για Vercel
+module.exports = app;
